ba3bb7e300b62125077bf12509c9fef9edd26bb8
Zhaoyu Qiao <54387352+zhaoyu-qiao@users.noreply.github.com>
2020-02-24 17:54:52 -0500

    update formatting of Saved.js

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,15 +1,8 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import {
-  Container
-} from "../components/Grid";
-import {
-  List,
-  ListItem
-} from "../components/List";
+import { Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
 
 class Saved extends Component {
   state = {
@@ -22,9 +15,11 @@ class Saved extends Component {
 
   loadBooks = () => {
     API.getBooks()
-      .then(res => this.setState({
-        books: res.data
-      }))
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
       .catch(err => console.log(err));
   };
 
@@ -35,59 +30,40 @@ class Saved extends Component {
   };
 
   render() {
-    return ( <
-      div >
-      <
-      hr style = {
-        {
-          border: "solid transparent"
-        }
-      }
-      />
-
-      <
-      Jumbotron >
-      <
-      h1 > Books On My List < /h1> <
-      /Jumbotron> <
-      Container fluid > {
-        this.state.books.length ? ( <
-          List > {
-            this.state.books.map(book => ( <
-              ListItem key = {
-                book._id
-              }
-              title = {
-                book.title
-              }
-              author = {
-                book.author
-              }
-              href = {
-                book.href
-              }
-              thumbnail = {
-                book.thumbnail
-              }
-              description = {
-                book.description
-              }
-              deleteBook = {
-                () => this.deleteBook(book._id)
-              }
-              />
-            ))
-          } <
-          /List>
-        ) : ( <
-          h1 id = "message"
-          className = "text-center" >
-          No Results to Display <
-          /h1>
-        )
-      } <
-      /Container> <
-      /div>
+    return (
+      <div>
+        <hr
+          style={{
+            border: "solid transparent"
+          }}
+        />
+        <Jumbotron>
+          <h1> Books On My List </h1>{" "}
+        </Jumbotron>{" "}
+        <Container fluid>
+          {" "}
+          {this.state.books.length ? (
+            <List>
+              {" "}
+              {this.state.books.map(book => (
+                <ListItem
+                  key={book._id}
+                  title={book.title}
+                  author={book.author}
+                  href={book.href}
+                  thumbnail={book.thumbnail}
+                  description={book.description}
+                  deleteBook={() => this.deleteBook(book._id)}
+                />
+              ))}{" "}
+            </List>
+          ) : (
+            <h1 id="message" className="text-center">
+              No Results to Display{" "}
+            </h1>
+          )}{" "}
+        </Container>{" "}
+      </div>
     );
   }
 }
